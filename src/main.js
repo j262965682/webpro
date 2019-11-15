d9035b7882ba318f50f532b11a0b3897bbc0d4ad
your name <your email>
2019-11-15 16:23:30 +0800

    project init

--- src/main.js
+++ src/main.js
@@ -1,10 +1,15 @@
+/* eslint-disable */
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastClick from 'fastclick'
+import './assets/styles/reset.css'
+import './assets/styles/border.css'
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
@@ -13,3 +18,5 @@ new Vue({
   components: { App },
   template: '<App/>'
 })
+
+//  路由就是根据网址不同返回不用的页面
